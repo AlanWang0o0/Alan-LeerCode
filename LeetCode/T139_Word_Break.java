e7b21d5b870bca49b7954385fdda15982d529654
AlanWang0o0 <54050499+AlanWang0o0@users.noreply.github.com>
2020-06-25 22:24:51 +0800

    2020/06/25

--- LeetCode/T139_Word_Break.java
+++ LeetCode/T139_Word_Break.java
@@ -1,6 +1,6 @@
 package LeetCode;
 
-import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,17 +8,17 @@ public class T139_Word_Break {
     public static void main(String[] args){
         System.out.println(wordBreak("aaaaaaa", Arrays.asList("aaa","aaaa")));
     }
-    
+
     public static boolean wordBreak(String s, List<String> wordDict){
         Boolean[] dp = new Boolean[s.length()+1];
         dp[0] = true;
         for (int i = 1; i < dp.length; i++) {
             dp[i] = false;
         }
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i+1; j <= s.length() ; j++) {
-                if(dp[i] && wordDict.contains(s.substring(i,j))){
-                    dp[j] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordDict.contains(s.substring(j, i))) {
+                    dp[i] = true;
                     break;
                 }
             }
